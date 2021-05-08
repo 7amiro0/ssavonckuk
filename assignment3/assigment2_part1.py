de6d12916666af81afec1254c461b3275b36b9eb
Stas <stas2007q@gmail.com>
2021-05-08 15:10:20 +0300

    works of errors

--- assignment3/assigment2_part1.py
+++ assignment3/assigment2_part1.py
@@ -7,25 +7,24 @@ def main():
     """Works function."""
     with urllib.request.urlopen('https://inventwithpython.com/dictionary.txt') as resp:
         link = set(resp.read().decode('utf-8').lower().split())
-    word_list = []
+
+    word_list = set()
 
     for word in link:
         rever_word = word[::-1]
-        for len_ in range(0, len(word)):
-            if word[len_:] in word_list or word[len_:] == word[len_::-1]:
-                if len_ != len(word):
-                    word_list.append(word)
-                    word_list.append(rever_word)
-                continue
-            if rever_word[len_:] in word_list or rever_word[len_:] == rever_word[len_::-1]:
-                if len_ != len(word):
-                    word_list.append(word)
-                    word_list.append(rever_word)
+
+        for len_ in range(len(word)):
+            if word[len_:] == word[len_:][::-1] and word[:len_:][::-1] in link:
+                word_list.add(word + " " + word[:len_:][::-1])
+            if rever_word[len_ + 1:] == rever_word[len_ + 1::][::-1] and rever_word[:len_ + 1:] in link:
+                word_list.add(rever_word[:len_ + 1:] + " " + word)
+
+
+    print(len(word_list))
     my_file = open('record.txt', 'w')
-    word_list.sort()
+
     for word in set(word_list):
         my_file.write(str(word) + '\n')
-
     my_file.close()
 
 
