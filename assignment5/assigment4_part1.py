07d1260c82cdaf97be1793d140c9816307af0400
Stas <stas2007q@gmail.com>
2021-05-11 23:42:40 +0300

    work 5

--- assignment5/assigment4_part1.py
+++ assignment5/assigment4_part1.py
@@ -1,14 +1,31 @@
-from collections import defaultdict
-word = 'txt word init hello bro nine four samir'
+word = 'txt word init hello bro nine four samir nikita artem roma andrey lox'
+a = 0
 win = []
-list_ = defaultdict(list)
+list1 = []
+list2 = []
+list3 = []
+list4 = ['1', '2', '3', '4', '5']
 for w in word.split():
-    if len(list_[1]) < 5:
-        list_[1].append(w)
-        if len(list_[1]) > 5:
-            list_[2].append(w)
-print(list_)
-
-
-
+    if len(list1) < 5:
+        list1.append(w)
+        continue
+    elif len(list1) == 5 and len(list2) != 5:
+        list2.append(w)
+        continue
+    elif len(list2) == 5 and len(list3) != 5:
+        list3.append(w)
+        continue
+for z in list1:
+    win.append(z)
+    continue
+for x in list2:
+    win.append(x)
+    continue
+for c in list3:
+    win.append(c)
+    continue
+for v in list4:
+    win.append(v)
+    continue
+print(win)
 
