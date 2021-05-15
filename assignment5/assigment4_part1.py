62114a4fbc15da9496c01a1692f8ae5edf75bd55
Stas <stas2007q@gmail.com>
2021-05-15 22:19:19 +0300

    finish work

--- assignment5/assigment4_part1.py
+++ assignment5/assigment4_part1.py
@@ -1,40 +1,51 @@
-word = 'txt word init hello bro nine four samir nikita artem roma andrey lox'
-key = input('enter your key')
-key = key.split(",")
-keys = []
+"""home works 5."""
+choice = input('choice 1 or 2, 1 = encrypt 2 =  decrypt: ')
 
-for number in key:
-    if int(number) < 0:
-        keys.append('-1')
-    else:
-        keys.append('0')
-    key = key.pop('-')
+if choice == '1':
+    word = input('enter your message: ')
+    word = word.split()
+    key = input('enter your key: ')
+    key = key.split(",")
 
-win = set()
-list1 = []
-list2 = []
-list3 = []
-list4 = ['1', '2', '3', '4', '5']
+    column = 5
+    list_ = []
+    text = []
 
-for w in word.split():
-    if len(list1) < 5:
-        list1.append(w)
-        continue
-    elif len(list1) == 5 and len(list2) != 5:
-        list2.append(w)
-        continue
-    elif len(list2) == 5 and len(list3) != 5:
-        list3.append(w)
-        continue
+    for i in range(column):
+        list_.append([])
+    for w in range(len(word)):
+        list_[w % column].append(word[w])
+    for ke in key:
+        if int(ke) < 0 and list_[abs(int(ke)) - 1] not in text:
+            text += list_[abs(int(ke)) - 1][::-1]
+        elif int(ke) > 0 and list_[abs(int(ke)) - 1] not in text:
+            text += list_[abs(int(ke)) - 1]
 
-for z in list1[::int(keys[0])]:
-    for x in list2[::int(keys[1])]:
-        for c in list3[::int(keys[2])]:
-            for v in list4:
-                if z not in win:
-                    win.add(z)
-                    win.add(x)
-                    win.add(c)
-                    win.add(v)
+    print(text)
+elif choice == "2":
 
-print(win)
+    word = input('enter your secret message: ')
+    word = word.split()
+    key = input('enter your key: ')
+    key = key.split(",")
+
+    column = 5
+    list_ = []
+    text = []
+    result_text = ''
+
+    for i in range(column):
+        list_.append([])
+    for w in range(len(word)):
+        list_[w % column].append(word[w])
+
+    for ke in key:
+        if int(ke) < 0 and list_[abs(int(ke)) - 1] not in text:
+            text += list_[abs(int(ke)) - 1][::-1]
+        elif int(ke) > 0 and list_[abs(int(ke)) - 1] not in text:
+            text += list_[abs(int(ke)) - 1]
+
+    for i in range(len(word)):
+        result += text[i] + ' '
+
+    print(result)
