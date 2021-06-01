2a425629b66988fc26aebc87e3023ac89064c940
Stas <stas2007q@gmail.com>
2021-06-01 15:44:30 +0300

    50/50 normal version

--- assignment7/assigment7_part1.py
+++ assignment7/assigment7_part1.py
@@ -1,210 +1,50 @@
 word = 'THIS OFF DETAINED ASCERTAIN WAYLAND CORRESPONDENTS OF AT WHY AND IF FILLS IT YOU GET THEY NEPTUNE THE TRIBUNE PLEASE ARE THEM CAN UP'
 word = word.split()
 open_text = 'correspondents of the Tribune wayland at neptune please ascertain why they are detained and get them off if you can this fills it up'
+for c in range(2, 12):
+    if len(word) % c == 0:
+        COLUMN = c
+        key = []
+        for r in range(2, 12):
+            if COLUMN * r == len(word):
+                ROW = r
+                rever = -1
+                l = []
+                q = []
+                for k in range(1, COLUMN):
+                    rever = -rever
+                    l.append(k * rever)
+                    q.append(k * -rever)
 
-key = [[-1, 2], [1, -2], [-2, 1], [2, -1]]
+                b = l[::-1]
+                c = q[::-1]
+                key.append(l)
+                key.append(q)
+                key.append(b)
+                key.append(c)
+                list_ = []
+                x = 0
 
-column = 2
-row = 12
-list_ = []
-x = 0
+                for i in range(COLUMN):
+                    list_.append([])
+                for w in range(len(word)):
+                    if len(list_[x]) != ROW:
+                        list_[x].append(word[w])
+                    else:
+                        x += 1
+                        list_[x].append(word[w])
 
-for i in range(column):
-    list_.append([])
-for w in range(len(word)):
-    if len(list_[x]) != row:
-        list_[x].append(word[w])
-    else:
-        x += 1
-        list_[x].append(word[w])
+                for ke in key:
+                    text = []
+                    result_text = ''
+                    for k in ke:
+                        if k < 0:
+                            text.append(list_[abs(k) - 1][::-1])
+                        else:
+                            text.append(list_[abs(k) - 1])
 
-for ke in key:
-    text = []
-    result_text = ''
-    for k in ke:
-        if k < 0:
-            text.append(list_[abs(k) - 1][::-1])
-        else:
-            text.append(list_[abs(k) - 1])
+                    for i in range(ROW):
+                        for c in range(COLUMN - 1):
+                            result_text += text[c][i] + ' '
 
-
-    for i in range(row):
-        result_text += text[0][i] + ' '
-        result_text += text[1][i] + ' '
-
-    print(result_text.lower(), ke)
-
-key = [[-1, 2, -3], [1, -2, 3], [3, -2, 1], [-3, 2, -1]]
-
-column = 3
-row = 8
-list_ = []
-x = 0
-
-for i in range(column):
-    list_.append([])
-for w in range(len(word)):
-    if len(list_[x]) != row:
-        list_[x].append(word[w])
-    else:
-        x += 1
-        list_[x].append(word[w])
-
-for ke in key:
-    text = []
-    result_text = ''
-    for k in ke:
-        if k < 0:
-            text.append(list_[abs(k) - 1][::-1])
-        else:
-            text.append(list_[abs(k) - 1])
-
-
-    for i in range(row):
-        result_text += text[0][i] + ' '
-        result_text += text[1][i] + ' '
-        result_text += text[2][i] + ' '
-
-    print(result_text.lower(), ke)
-key = [[-1, 2, -3, 4], [1, -2, 3, -4], [-4, 3, -2, 1], [4, -3, 2, -1]]
-
-if len(key) == 0 or len(word) == 0:
-    input('Pleas pres enter of exit program')
-    exit()
-
-column = 4
-row = 6
-list_ = []
-x = 0
-
-for i in range(column):
-    list_.append([])
-for w in range(len(word)):
-    if len(list_[x]) != row:
-        list_[x].append(word[w])
-    else:
-        x += 1
-        list_[x].append(word[w])
-
-for ke in key:
-    text = []
-    result_text = ''
-    for k in ke:
-        if k < 0:
-            text.append(list_[abs(k) - 1][::-1])
-        else:
-            text.append(list_[abs(k) - 1])
-
-
-    for i in range(row):
-        result_text += text[0][i] + ' '
-        result_text += text[1][i] + ' '
-        result_text += text[2][i] + ' '
-        result_text += text[3][i] + ' '
-
-    print(result_text.lower(), ke)
-
-key = [[-1, 2, -3, 4, -5, 6], [1, -2, 3, -4, 5, -6], [-6, 5, -4, 3, -2, 1], [6, -5, 4, -3, 2, -1]]
-
-column = 6
-row = 4
-list_ = []
-x = 0
-
-for i in range(column):
-    list_.append([])
-for w in range(len(word)):
-    if len(list_[x]) != row:
-        list_[x].append(word[w])
-    else:
-        x += 1
-        list_[x].append(word[w])
-
-for ke in key:
-    text = []
-    result_text = ''
-    for k in ke:
-        if k < 0:
-            text.append(list_[abs(k) - 1][::-1])
-        else:
-            text.append(list_[abs(k) - 1])
-
-
-    for i in range(row):
-        result_text += text[0][i] + ' '
-        result_text += text[1][i] + ' '
-        result_text += text[2][i] + ' '
-        result_text += text[3][i] + ' '
-
-
-    print(result_text.lower(), ke)
-
-key = [[-1, 2, -3, 4, -5, 6, -7, 8], [1, -2, 3, -4, 5, -6, 7, -8], [-8, 7, -6, 5, -4, 3, -2, 1], [8, -7, 6, -5, 4, -3, 2, -1]]
-
-column = 8
-row = 3
-list_ = []
-x = 0
-
-for i in range(column):
-    list_.append([])
-for w in range(len(word)):
-    if len(list_[x]) != row:
-        list_[x].append(word[w])
-    else:
-        x += 1
-        list_[x].append(word[w])
-
-for ke in key:
-    text = []
-    result_text = ''
-    for k in ke:
-        if k < 0:
-            text.append(list_[abs(k) - 1][::-1])
-        else:
-            text.append(list_[abs(k) - 1])
-
-
-    for i in range(row):
-        result_text += text[0][i] + ' '
-        result_text += text[1][i] + ' '
-        result_text += text[2][i] + ' '
-        result_text += text[3][i] + ' '
-
-
-    print(result_text.lower(), ke)
-
-key = [[-1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12], [1, -2, 3, -4, 5, -6, 7, -8, 9, -10, 11, -12], [-12, -11, -10, 9, -8, 7, -6, 5, -4, 3, -2, 1], [12, -11, 10, -9, 8, -7, 6, -5, 4, -3, 2, -1]]
-
-column = 12
-row = 2
-list_ = []
-x = 0
-
-for i in range(column):
-    list_.append([])
-for w in range(len(word)):
-    if len(list_[x]) != row:
-        list_[x].append(word[w])
-    else:
-        x += 1
-        list_[x].append(word[w])
-
-for ke in key:
-    text = []
-    result_text = ''
-    for k in ke:
-        if k < 0:
-            text.append(list_[abs(k) - 1][::-1])
-        else:
-            text.append(list_[abs(k) - 1])
-
-
-    for i in range(row):
-        result_text += text[0][i] + ' '
-        result_text += text[1][i] + ' '
-        result_text += text[2][i] + ' '
-        result_text += text[3][i] + ' '
-
-
-    print(result_text.lower(), ke)
+                    print(result_text.lower(), ke)
