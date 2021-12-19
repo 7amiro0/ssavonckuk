feaaa5d3b3ff0d4e8359e368cc7c5f582a904c9c
7amiro0 <81709792+7amiro0@users.noreply.github.com>
2021-12-19 09:59:10 +0200

    rename

--- assignment6/assigment6_part1.py
+++ assignment6/assigment6_part1.py
@@ -8,10 +8,7 @@ words = """Here you see your text that you want to decrypt."""
 """After this #========#, do not touch or change anything."""
 # ======================================================================================================================#
 
-"""text no have space
-        |
-        V"""
-words = words.upper()
+words = words.upper() """<- text no have space"""
 words = words.split()
 big_word = ''.join(words)
 
@@ -19,22 +16,13 @@ more_later = [[], []]
 result = ''
 space_in_txt = []
 
-"""first later in text
-        |
-        V"""
-for later in big_word[::2]:
+for later in big_word[::2]: #<- first later in text
     more_later.append(later)
 
-"""second later in text
-        |
-        V"""
-for later in big_word[1::2]:
+for later in big_word[1::2]: #<- first later in text
     more_later.append(later)
 
-"""replase letters at their positions
-            |
-            V"""
-for later in more_later[0] + more_later[1]:
+for later in more_later[0] + more_later[1]: #<- replase letters at their positions
     space_in_txt.append(later)
 for i in range(len(space_in_txt)):
     if (i + 1) % 5 == 0:
