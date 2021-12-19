018f0be7fd9f805c7d8b224ce5c71b065af3d268
7amiro0 <81709792+7amiro0@users.noreply.github.com>
2021-12-19 10:07:33 +0200

    rename

--- assignment8/assigment8_part1/decrypt.py
+++ assignment8/assigment8_part1/decrypt.py
@@ -1,13 +1,9 @@
 """After # ----------- # do not touch anything"""
-"""Here is the text to be encrypted
-                   |
-                   V"""
+"""Here is the text to be encrypted"""
 words = "your text pleas"
 #----------------------------------------------------------------------------------------------------------------------#
 
-"""text no have space
-        |
-        V"""
+"""text no have space"""
 words = words.split()
 big_word = ''.join(words)
 
@@ -16,33 +12,23 @@ split = int(len(big_word) / 3)
 more_later = [[], [], []]
 resoult = ''
 
-"""first later in text
-        |
-        V"""
+"""first later in text"""
 for later in big_word[:split:]:
     more_later[0].append(later)
 
-"""second later in text
-        |
-        V"""
+"""second later in text"""
 for later in big_word[split:split + split]:
     more_later[1].append(later)
 
-"""third later in text
-        |
-        V"""
+"""third later in text"""
 for later in big_word[split + split:]:
     more_later[2].append(later)
 
-"""inserts letters at their positions
-            |
-            V"""
+"""inserts letters at their positions"""
 for i in range(len(more_later[0])):
     resoult += ''.join(more_later[0][i])
     resoult += ''.join(more_later[1][i])
     resoult += ''.join(more_later[2][i])
 
-"""outputs the result
-    |
-    V"""
+"""outputs the result"""
 print(resoult)
